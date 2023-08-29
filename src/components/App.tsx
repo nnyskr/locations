2b616580bc3d303351a0f31b832f8a86b8f3a351
nnyskr <nnyskr@icloud.com>
2023-08-30 01:55:53 +0200

    add alert message

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
 import Pages from './Pages';
 
 export default function App() {
+  useEffect(() => {
+    window.alert(
+      'Assumed there is only so little locations;\n\nIf there were more, would use virtualization :)'
+    );
+  }, []);
   return <Pages />;
 }

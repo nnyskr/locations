146ef05d762aa036520be32561cd585ae1177868
nnyskr <nnyskr@icloud.com>
2023-08-30 15:46:31 +0200

    update alert

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,8 @@ import Pages from './Pages';
 export default function App() {
   useEffect(() => {
     window.alert(
-      'Assumed there is only so little locations;\n\nIf there were more, would use virtualization :)'
+      `Assumed there is only so little locations - if there were more, would use virtualization;
+      \nCould not find semibold variant of Lato font, so typography is a bit off`
     );
   }, []);
   return <Pages />;

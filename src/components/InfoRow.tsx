db9db7a3b973b70b44f63ac46ebef4d9e2779415
nnyskr <nnyskr@icloud.com>
2023-08-30 18:44:10 +0200

    add default alt to img

--- src/components/InfoRow.tsx
+++ src/components/InfoRow.tsx
@@ -2,13 +2,19 @@ import '../styles/info-row.css';
 
 interface InfoProps {
   icon: string;
+  alt?: string;
   infoNode: React.ReactNode;
 }
 
-export default function InfoRow({ icon, infoNode, ...props }: InfoProps) {
+export default function InfoRow({
+  icon,
+  alt = '',
+  infoNode,
+  ...props
+}: InfoProps) {
   return (
     <div className="info-row" {...props}>
-      <img data-testid="info-row-icon" src={icon} />
+      <img data-testid="info-row-icon" src={icon} alt="icon" />
       <p data-testid="info-row-data" className="typography-s ellipsis">
         {infoNode}
       </p>
